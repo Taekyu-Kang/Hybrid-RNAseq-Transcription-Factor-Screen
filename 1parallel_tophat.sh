a41ef37385e0e19cf1e01b66674cd1685727caeb
Taekyu Kang <30639249+Taekyu-Kang@users.noreply.github.com>
2022-11-04 10:47:10 -0700

    Add files via upload

--- 1parallel_tophat.sh
+++ 1parallel_tophat.sh
@@ -6,8 +6,8 @@ LIBTYPE=$3 #library type, fr-unstranded, fr-firststrand, fr-secondstrand
 INDEX=$4 #location of bowtie index, ending with index name
 
 
-for i in $(ls $DIR*.fq.gz | rev | cut -d"/" -f1 | rev | cut -d"_" -f1 | uniq)  #grabs all unique file identifiers after last '/' and before last '_'
+for i in $(ls $DIR*.fq | rev | cut -d"/" -f1 | rev | cut -d"_" -f1 | uniq)  #grabs all unique file identifiers after last '/' and before last '_'
 do
-	echo ${i}
-	tophat -o ${OUTDIR}${i}/ -N 0 --library-type ${LIBTYPE} ${INDEX} ${DIR}${i}_1.fq.gz ${DiR}${i}_2.fq.gz &
+	echo ${i} #${OUTDIR}${i}/ ${LIBTYPE} ${INDEX} ${DIR}${i}_1.fq ${DIR}${i}_2.fq
+	tophat -o ${OUTDIR}${i}/ -N 0 --library-type ${LIBTYPE} ${INDEX} ${DIR}${i}_1.fq ${DIR}${i}_2.fq &
 done
