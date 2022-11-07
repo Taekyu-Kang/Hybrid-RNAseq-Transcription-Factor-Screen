2a657f500e911208c77f8b7f69dc7ec6f44d1700
Taekyu Kang <30639249+Taekyu-Kang@users.noreply.github.com>
2022-11-07 09:41:00 -0800

    Add files via upload

--- 4parallel_samtoolsview.sh
+++ 4parallel_samtoolsview.sh
@@ -8,5 +8,5 @@ QUAL=$3 #minimum MAPQ to keep
 for i in $(ls $DIR*.fq | rev | cut -d"/" -f1 | rev | cut -d"_" -f1 | uniq)  #grabs all unique file identifiers after last '/' and before last '_'
 do
 	echo ${i}
-	samtools view -q ${QUAL} -O SAM ${OUTDIR}${i}/${i}.sam -o ${OUTDIR}${i}/unique_q${QUAL}.sam &
+	samtools view -q ${QUAL} -O SAM ${OUTDIR}${i}/sorted.bam -o ${OUTDIR}${i}/unique_q${QUAL}.sam &
 done
