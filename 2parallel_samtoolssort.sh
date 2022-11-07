dfdbcafa415d3a666fbca4509978e127fad5bdc2
Taekyu Kang <30639249+Taekyu-Kang@users.noreply.github.com>
2022-11-07 09:24:56 -0800

    Update 2parallel_samtoolssort.sh

--- 2parallel_samtoolssort.sh
+++ 2parallel_samtoolssort.sh
@@ -7,5 +7,5 @@ OUTDIR=$2
 for i in $(ls $DIR*.fq | rev | cut -d"/" -f1 | rev | cut -d"_" -f1 | uniq)  #grabs all unique file identifiers after last '/' and before last '_'
 do
 	echo ${i}
-	samtools sort -O BAM -o ${OUTDIR}${i}/sorted.bam -T ${OUTDIR}${i}/temp ${OUTDIR}${i}/${i}.sam &
+	samtools sort -O BAM -o ${OUTDIR}${i}/sorted.bam -T ${OUTDIR}${i}/temp ${OUTDIR}${i}/accepted_hits.bam &
 done
